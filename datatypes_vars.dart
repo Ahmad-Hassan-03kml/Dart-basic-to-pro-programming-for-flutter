c06cd032d6615f7b5b527d45dafd2a7018a86a59
Ahmad-Hassan-03kml <56421968+Ahmad-Hassan-03kml@users.noreply.github.com>
2021-02-08 10:34:38 -0800

    Add files via upload

---
+++ datatypes_vars.dart
@@ -0,0 +1,73 @@
+// exmample program to understand data types and variable declairation in dart
+
+void main()
+{
+  // now take a look on int
+
+//   int n1 = 10;
+//   var num1 = 10;
+
+// // look on doublle floating number
+// double n2=10.333;
+// var num2 = 10.333;
+
+// take a look at single and ,ulti line string 
+// String sline="single line string  in dart";
+// String mline=''' dart is fun for program 
+// new line 
+// ssssssss
+// ''';
+// var sline1="single line string  in dart";
+// var mline1=''' dart is fun for program 
+// new line 
+// ssssssss
+// ''';
+
+
+
+// now take a look at bool
+// bool flag = false;
+// var flag1= true;
+// print(num1);
+// print(num2);
+// print(sline1);
+// print(mline1);
+
+// // print(flag);
+// // print(flag1);
+
+
+
+
+// dynamic  var1 = 10;
+// print(var1);
+
+// var1= "string value";
+
+// //print( var1 );   // string value     >>       Te value of var 1 now string vlaue
+
+// print("now the value of var1 is     $var1   ");
+
+ // there are two ways of interpolation 
+ // when single value     use $
+ // when you have expression   use ${}
+
+
+// var num1= 100;
+// var num2 = 200;
+
+// print("the value of num1 ====>>     $num1");
+// print("the value of num2 ====>>     ${num2}");
+
+
+// print("the value of num1+num2+10-19 ====>>     ${num1+num2+10-19}  ");
+
+
+
+
+
+                    //            section  2.1  LIST MAP SET in Dart
+
+
+
+}
