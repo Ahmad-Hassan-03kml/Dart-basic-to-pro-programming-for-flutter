473bfd90d7d97f891f73374bfe0fe4f064a8eab3
Ahmad-Hassan-03kml <56421968+Ahmad-Hassan-03kml@users.noreply.github.com>
2021-02-08 02:43:56 -0800

    Add files via upload

---
+++ hello.dart
@@ -0,0 +1,21 @@
+//  cretae a main method entry point of dart program
+
+void main()
+{
+// to print any text in dart 
+print('hello   world   '); 
+
+ print("hello humanity   ojsdhfjsdhfkjsdf      ");
+
+
+print(''' hello dear viewrs  
+new line 1 
+new line 1
+sd
+d
+d
+d
+d  ''');
+
+
+}
