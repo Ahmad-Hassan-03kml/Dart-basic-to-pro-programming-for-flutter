9f1f2b3891b0009bc2a72687c527b96872bb3dae
Ahmad-Hassan-03kml <56421968+Ahmad-Hassan-03kml@users.noreply.github.com>
2021-02-12 09:00:49 -0800

    Add files via upload

---
+++ const.dart
@@ -0,0 +1,32 @@
+// 
+void main(){
+
+// first of all create a local variable 
+
+var x = 20;
+print(x);   // 20;
+
+x++;  //  1 increment
+
+print(x); // 21;
+
+final int f1= 10;
+print(f1);
+
+f1++;
+
+
+// another way 
+final f2 =11;
+print(f2); // 11
+
+
+
+const String c1 ="contant";
+print(c1);
+c1="new value";
+
+const c2 = "constant2";
+print(c2);
+
+}// end of main
