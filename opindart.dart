57ee2a2f057acd4ce7b1d9ffce6489921ea194a5
Ahmad-Hassan-03kml <56421968+Ahmad-Hassan-03kml@users.noreply.github.com>
2021-02-11 22:02:19 -0800

    Add files via upload

---
+++ opindart.dart
@@ -0,0 +1,120 @@
+void main()
+{
+// take a look at arthimetic ops
+
+// var a = 10;
+// var b =20;
+
+// var add= a+b;
+// var sub= a-b;
+// var mul=a*b;
+// var div=a/b;
+// var intdiv= a~/b;
+
+// print("The Adition of  a and b =  ${add} \n");
+// print("The Subtraction of  a and b =  ${sub} \n");
+// print("The Multipl of  a and b =  ${mul} \n");
+// print("The Division of  a and b =  ${div} \n");
+// print("The integer ans Division of  a and b =  ${intdiv} \n");
+
+
+
+// int a = 2; 
+//     int b = 3; 
+  
+    
+//     var c = a > b; 
+//     print("a is greater than b is $c"); 
+  
+//     // Smaller between a and b 
+//             //2 < 3
+//     var d = a < b; 
+//     print("a is smaller than b is $d"); 
+  
+//     // Greater than or equal to between a and b 
+//     var e = a >= b; 
+//     print("a is greater than b is $e"); 
+  
+//     // Less than or equal to between a and b 
+//     var f = a <= b; 
+//     print("a is smaller than b is $f"); 
+  
+//     // Equality between a and b 
+//     var g = b == a; 
+//     print("a and b are equal is $g"); 
+  
+//     // Unequality between a and b 
+//     var h = b != a; 
+//     print("a and b are not equal is $h");
+
+//     if(a < b)
+//     {
+//       print("a is lessthen b");
+
+//     }
+
+// if(a > b)
+//     {
+//       print("a is greater then b");
+
+//     }if(a != b)
+//     {
+//       print("a is not equal to b b");
+
+//     }
+
+
+
+
+// var v1 = 10;   // which is int
+
+// var v2="string";  // wich is string
+
+// print(v1 is int);  // ?        true
+// print(v1 is! double);   //   false
+
+// print(v1 is String);  // true;
+
+// print(v2 is int);  ///  false
+// print(v2 is String);  // true 
+
+
+//  var a = 5; 
+//   var b = 7; 
+//   var c = a+b;
+//   print(c); // 12
+     
+//   var d;  // now d is null
+
+//   d  ??= a + b; // Value is assign as it is null 
+//     print(d); // 12
+//     // now d is not null 
+
+//     // Again trying to assign value to d 
+//     d ??= a - b; // Value is not assign as it is not null 
+    
+//     print(d); // 12
+
+
+// now ytake alook at conditinal op.
+
+int a = 5; 
+int b = 7; 
+  
+    // Conditional Statement 
+    var c = (a < b) ? "assigned" : "not assigned"; 
+    print(c); 
+  
+    // Condtional statement 
+    int n; 
+    var d = n?? " assined"; 
+    print(d); 
+  
+    // After assigning value to n 
+    n = 10; 
+    d = n ?? "not assigned"; 
+    print(d);
+
+
+
+}
